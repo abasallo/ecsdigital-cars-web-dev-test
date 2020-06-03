0a8fd3f80fe145c999c6b05edccdac2636cea148
Alvaro Basallo <alvaro@basallo.es>
2020-06-03 10:56:32 +0200

    Deeper test (with mocked external call) added for data-muse.js

--- src/modules/data-muse.test.js
+++ src/modules/data-muse.test.js
@@ -6,10 +6,14 @@ import { expect } from '@jest/globals'
 
 import { fewWordsSoundingLike } from './data-muse'
 
+import axios from 'axios'
+
+jest.mock('axios')
+
 test('Should return a short paragraph with words sounding similar to the provided', async () => {
+  axios.get.mockImplementationOnce((_) => ({ data: [{ word: _ }] }))
+
   const similarSoundingParagraph = await fewWordsSoundingLike('test')
-  expect(similarSoundingParagraph.length).toBeGreaterThan(0)
-  expect(similarSoundingParagraph.length).toBeLessThanOrEqual(
-    Number(process.env.WORDS_SOUNDING_SIMILAR_TO_MODEL_MAX_PARAGRAPH_LENGTH) || constants.DEFAULT_MAX_PARAGRAPH_LENGTH
-  )
+
+  expect(similarSoundingParagraph).toBe(`${constants.WORDS_SOUNDING_SIMILAR_TO_MUSE_URL}test`)
 })
